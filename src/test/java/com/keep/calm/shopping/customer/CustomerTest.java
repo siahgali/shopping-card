f2e829411914e068837dd434cebe448d2d0433de
siminbigdeli <siminbigdelii@gmail.com>
2022-08-08 22:33:40 -0400

    update customer unit test

--- src/test/java/com/keep/calm/shopping/customer/CustomerTest.java
+++ src/test/java/com/keep/calm/shopping/customer/CustomerTest.java
@@ -4,14 +4,14 @@ import com.keep.calm.shopping.address.Address;
 import com.keep.calm.shopping.cart.Cart;
 import com.keep.calm.shopping.helper.JsonFileName;
 import com.keep.calm.shopping.helper.JsonHelper;
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 
-public class CustomerTest extends TestCase {
+public class CustomerTest {
     @Before
     public void setUp() {
         JsonHelper.clearJsonFile(JsonFileName.CUSTOMER);
@@ -19,26 +19,48 @@ public class CustomerTest extends TestCase {
     @Test
     public void Should_addCustomerToJsonFile_when_noError(){
         //Initial GIVEN
-        Customer customer = buildCustomer();
+        Customer customer = buildCustomer("simin");
 
         //WHEN
         customer.addCustomer();
 
         //then
         List<Customer> customerList = JsonHelper.convertJsonFileToJavaObject(JsonFileName.CUSTOMER);
-        assertEquals(1, customerList.size());
+        assertEquals(1,customerList.size());
+
 
     }
 
-    private Customer buildCustomer() {
+
+  /*  @Test
+    public void Should_updateCustomerToJsonFile_when_noError(){
+        //Initial GIVEN
+        Customer customer = buildCustomer("Akbar");
+       // Customer customer = buildCustomer("Simin");
+
+        //WHEN
+        customer.updateCustomer();
+
+        //then
+        List<Customer> customerList = JsonHelper.convertJsonFileToJavaObject(JsonFileName.CUSTOMER);
+        assertEquals("Akbar",customer.getFirstName());
+
+
+    }
+*/
+    private Customer buildCustomer(String name) {
         Customer customer = new Customer();
         customer.setCustomerId("1");
         customer.setCustomerPhone(" 4169887806");
-        customer.setFirstName("simin");
+        customer.setFirstName(name);
         customer.setLastName("Bigdeli");
         Address buildingAddress = new Address();
         customer.setBillingAddress(buildingAddress);
+        customer.setShippingAddress(buildingAddress);
         Cart cart = new Cart();
+        cart.setCartId("1");
+        cart.setTotalPrice(1000d);
+
         customer.setCart(cart);
         return customer;
     }
